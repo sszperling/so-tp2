e8de4a0e78bf884f66800b797be6dc7b636f7999
Alexis Balbachan <alexisbalbachan@gmail.com>
2017-10-23 19:58:20 -0300

    Casi todos los metodos (estan bien?) menos addAndInc

--- consola.cpp
+++ consola.cpp
@@ -6,11 +6,18 @@
 #include <list>
 #include <iostream>
 #include <sstream>
+#include <queue>
 #include "consola.hpp"
 #include "HashMap.hpp"
 #include "mpi.h"
 
 using namespace std;
+
+//0 = req
+//1 = rel
+//2 = ack
+//3 = relAll
+//4 = ready
 
 #define CMD_LOAD    "load"
 #define CMD_ADD     "addAndInc"
@@ -23,35 +30,125 @@ static unsigned int np;
 
 // Crea un ConcurrentHashMap distribuido
 static void load(list<string> params) {
+	MPI_Status Stat;
 
+	bool message;
+	queue<unsigned int> freeNodes;
+
+	//All nodes are ready before starting
+	for (unsigned int i = 1; i < np; i++){
+		freeNodes.push(i+1);
+	}
+
+	//For each file find a ready node to send it to
     for (list<string>::iterator it=params.begin(); it != params.end(); ++it) {
-       // TODO: Implementar
+		if (freeNodes.empty()){
+			MPI_Recv(&message, 1, MPI_C_BOOL, MPI_ANY_SOURCE, MPI_ANY_TAG, MPI_COMM_WORLD, &Stat);
+			freeNodes.push(Stat.MPI_SOURCE);
+		}
+
+		string command = (char)1 + *it;
+		MPI_Send(command.c_str(), command.length()+1, MPI_CHAR, freeNodes.front(), 0, MPI_COMM_WORLD);
+		freeNodes.pop();
     }
+
+	//Wait for all nodes to be ready
+	while(freeNodes.size() < np-1){
+		MPI_Recv(&message, 1, MPI_C_BOOL, MPI_ANY_SOURCE, MPI_ANY_TAG, MPI_COMM_WORLD, &Stat);
+		freeNodes.push(Stat.MPI_SOURCE);
+	}
 
     cout << "La listá esta procesada" << endl;
 }
 
 // Esta función debe avisar a todos los nodos que deben terminar
 static void quit() {
-    // TODO: Implementar
+	char command=0; //EXIT
+
+	//BCAST
+	MPI_Request reqs[np-1];   // required variable for non-blocking calls
+   	MPI_Status stats[np-1];   // required variable for Waitall routine
+	for (unsigned int i = 1; i < np; i++){
+		MPI_Isend(&command, 1, MPI_CHAR, i, 0, MPI_COMM_WORLD, &reqs[i-1]);
+	}
+	MPI_Waitall(np-1, reqs, stats);
 }
 
 // Esta función calcula el máximo con todos los nodos
 static void maximum() {
-    pair<string, unsigned int> result;
+	char command=3; //MAXIMUM
 
-    // TODO: Implementar
-    string str("a");
-    result = make_pair(str,10);
+	MPI_Status Stat;
+	unsigned int remainingNodes = np-1;
+	HashMap tmpMap;
 
-    cout << "El máximo es <" << result.first <<"," << result.second << ">" << endl;
+	//BCAST
+	MPI_Request reqs[np-1];   // required variable for non-blocking calls
+	MPI_Status stats[np-1];   // required variable for Waitall routine
+	for (unsigned int i = 1; i < np; i++){
+		MPI_Isend(&command, 1, MPI_CHAR, i, 0, MPI_COMM_WORLD, &reqs[i-1]);
+	}
+	MPI_Waitall(np-1, reqs, stats);
+
+	//RECIEVE NODE WORDS
+	while(remainingNodes > 0){
+		MPI_Probe(MPI_ANY_SOURCE, MPI_ANY_TAG, MPI_COMM_WORLD, &Stat);
+		int message_length;
+		MPI_Get_count(&Stat, MPI_CHAR, &message_length);
+		char message[message_length];
+		MPI_Recv(&message, message_length, MPI_CHAR, MPI_ANY_SOURCE, MPI_ANY_TAG, MPI_COMM_WORLD, &Stat);
+
+		if (message[0] > 0){	//NODE FINISHED PROCESSING
+			remainingNodes--;
+		}
+		if (message[0] == 2){	//NODE WAS EMPTY
+			continue;
+		}
+
+		string word = string(&message[1]);
+
+		pair<string, unsigned int> wordPair (word, Stat.MPI_TAG); // <word, count>
+		for (unsigned int i = 0; i < wordPair.second; i++){
+			tmpMap.addAndInc(word);
+		}
+	}
+
+	if (tmpMap.size() > 0){
+		pair<string, unsigned int> result = tmpMap.maximum();
+		cout << "El máximo es <" << result.first <<"," << result.second << ">" << endl;
+	}else{
+		cout << "No hay elementos en el HashMap" << endl;
+	}
+
+
 }
 
 // Esta función busca la existencia de *key* en algún nodo
 static void member(string key) {
-    bool esta = false;
+	string command = (char)2 + key; //MEMBER CMD + KEY
 
-    // TODO: Implementar
+	unsigned int remainingNodes = np-1;
+	MPI_Status Stat;
+
+	//BCAST
+	MPI_Request reqs[np-1];   // required variable for non-blocking calls
+   	MPI_Status stats[np-1];   // required variable for Waitall routine
+	for (unsigned int i = 1; i < np; i++){
+		MPI_Isend(command.c_str(), command.length()+1, MPI_CHAR, i, 0, MPI_COMM_WORLD, &reqs[i-1]);
+	}
+	MPI_Waitall(np-1, reqs, stats);
+	//END BCAST
+
+
+	//RECV RESPONSES
+	bool esta = false;
+	bool message;
+
+	while(remainingNodes > 0){
+		MPI_Recv(&message, 1, MPI_C_BOOL, MPI_ANY_SOURCE, MPI_ANY_TAG, MPI_COMM_WORLD, &Stat);
+		esta = (esta || message);
+		remainingNodes--;
+	}
 
     cout << "El string <" << key << (esta ? ">" : "> no") << " está" << endl;
 }
@@ -157,11 +254,16 @@ void consola(unsigned int np_param) {
     printf("  "CMD_MEMBER" <string>\n");
     printf("  "CMD_MAXIMUM"\n");
     printf("  "CMD_SQUIT"|"CMD_QUIT"\n");
-
+	//MPI_Status Stat;
+	//char inmsg;
+	//MPI_Probe(MPI_ANY_SOURCE, 1, MPI_COMM_WORLD, &Stat);
+	//printf(" ----------%d", Stat.MPI_SOURCE);
+	//MPI_Recv(&inmsg, 1, MPI_CHAR, source, tag, MPI_COMM_WORLD, &Stat);
     bool fin = false;
     while (!fin) {
         printf("> ");
         fflush(stdout);
         fin = procesar_comandos();
     }
+	cout << "Consola saliendo.." << endl;
 }
--- nodo.cpp
+++ nodo.cpp
@@ -3,17 +3,81 @@
 #include "mpi.h"
 #include <unistd.h>
 #include <stdlib.h>
+#include <string>
 
 using namespace std;
 
 void nodo(unsigned int rank) {
     printf("Soy un nodo. Mi rank es %d \n", rank);
+	HashMap myHashMap;
 
-    // TODO: Implementar
-    // Creo un HashMap loal
+	int clock_tag = 0;
 
     while (true) {
-        // TODO: Procesar mensaje
+		MPI_Status Stat;
+		MPI_Probe(MPI_ANY_SOURCE, MPI_ANY_TAG, MPI_COMM_WORLD, &Stat);
+		if (Stat.MPI_SOURCE == 0){	//RECIEVED COMMAND FROM CONSOLE
+
+			int message_length;
+			MPI_Get_count(&Stat, MPI_CHAR, &message_length);
+			char message[message_length];
+			MPI_Recv(&message, message_length, MPI_CHAR, MPI_ANY_SOURCE, MPI_ANY_TAG, MPI_COMM_WORLD, &Stat);
+			clock_tag = max(Stat.MPI_TAG, clock_tag+1);
+
+			if (message[0] == 0){		//EXIT
+				printf("Nodo %d saliendo..\n", rank);
+				return;
+
+			}else if (message[0] == 1){	//LOAD FILE
+				myHashMap.load(string(&message[1]));
+				printf("Nodo %d procesó archivo: '%s'\n", rank, &message[1]);
+				trabajarArduamente();
+
+				//Send ready msg
+				bool response = true;
+				MPI_Send(&response, 1, MPI_C_BOOL, 0, clock_tag, MPI_COMM_WORLD);
+
+			}else if (message[0] == 2){ //MEMBER
+
+				bool response = myHashMap.member(&message[1]);
+				if (response){
+					printf("Nodo %d SI encontró la clave: '%s'\n", rank, &message[1]);
+				}else{
+					printf("Nodo %d NO encontró la clave: '%s'\n", rank, &message[1]);
+				}
+				trabajarArduamente();
+				MPI_Send(&response, 1, MPI_C_BOOL, 0, clock_tag, MPI_COMM_WORLD);
+
+			}else if (message[0] == 3){	//MAXIMUM
+
+				trabajarArduamente();
+				string currentWord = "";
+				unsigned int currentCount = 0;
+				for (HashMap::iterator it=myHashMap.begin(); it != myHashMap.end(); ++it) {
+					if (currentWord == ""){
+						currentWord = *it;
+						currentCount = 0;
+					}
+
+					if (currentWord != *it){
+						string response = (char)0 + currentWord;
+						MPI_Send(response.c_str(), response.length()+1, MPI_CHAR, 0, currentCount, MPI_COMM_WORLD);
+						currentWord = *it;
+						currentCount = 1;
+					}else{
+						currentCount++;
+					}
+
+				}
+				int finalResponse = ((currentWord == "") + 1);
+				string response = (char)finalResponse + currentWord;
+				MPI_Send(response.c_str(), response.length()+1, MPI_CHAR, 0, currentCount, MPI_COMM_WORLD);
+				printf("Nodo %d terminó de enviar sus palabras\n", rank);
+
+			}else{						//SHOULD NOT HAPPEN
+				printf("WTF%d\n", message[0]);
+			}
+		}
     }
 }
 
